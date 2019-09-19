d2df34dd9bc72b0d05f7cf2b58b9fdd55d190e17
waruby <lolnoemail@lolilol.nl>
2019-09-20 01:24:25 +0200

    adds tests for bracket and const accessors

---
+++ tests/gic_const_get.cpp
@@ -0,0 +1,35 @@
+#include "generationally_indexed_container.hpp"
+#include <utility>
+
+using namespace genex;
+
+static const int VAL = 444;
+
+static int check_from_const(gic<int> const &container, gic<int>::key_type const &k) {
+    // testing call to get on a const gic
+    int const * maybe_val = container.get(k);
+
+    if(maybe_val == nullptr)
+        return 1;
+
+    if(*maybe_val != VAL)
+        return 2;
+
+    // testing call to the operator [] on a const gic
+    maybe_val = container[k];
+
+    if(maybe_val == nullptr)
+        return 3;
+
+    if(*maybe_val != VAL)
+        return 4;
+
+    return 0;
+}
+
+int main() {
+    gic<int> container;
+
+    auto key = container.emplace(VAL);
+    return check_from_const(container, key);
+}
