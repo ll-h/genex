718ae9659db40ecddbd70152ccc6eee8a0b74659
waruby <lolnoemail@lolilol.nl>
2019-11-03 22:27:14 +0100

    adds tests for some iterator concepts
    - InputIterator
    - OutputIterator
    - iterator default-constructibility (one of the requirements of
      the ForwardIterator concept)

--- tests/generic/gic_iterator_tests.hpp
+++ tests/generic/gic_iterator_tests.hpp
@@ -4,16 +4,17 @@
 #include <boost/test/unit_test.hpp>
 #include <utility>
 #include <algorithm>
+#include <type_traits>
 #include "generic_test_definitions.hpp"
 using namespace boost::unit_test;
+
 
 // ===== Iterator concept =====
 
 BOOST_AUTO_TEST_CASE( deref_first_allocated ) {
     gic_derived<int> container;
 
-    auto key = container.emplace(NON_ZERO_VAL);
-    (void)key;
+    (void)container.emplace(NON_ZERO_VAL);
     auto it = container.begin();
 
     BOOST_TEST(*it == NON_ZERO_VAL);
@@ -22,8 +23,7 @@ BOOST_AUTO_TEST_CASE( deref_first_allocated ) {
 BOOST_AUTO_TEST_CASE( deref_implicit_const ) {
     gic_derived<int> container;
 
-    auto key = container.emplace(NON_ZERO_VAL);
-    (void)key;
+    (void)container.emplace(NON_ZERO_VAL);
     auto it = std::as_const(container).begin();
 
     BOOST_TEST(*it == NON_ZERO_VAL);
@@ -32,8 +32,7 @@ BOOST_AUTO_TEST_CASE( deref_implicit_const ) {
 BOOST_AUTO_TEST_CASE( deref_explicit_const ) {
     gic_derived<int> container;
 
-    auto key = container.emplace(NON_ZERO_VAL);
-    (void)key;
+    (void)container.emplace(NON_ZERO_VAL);
     auto it = container.cbegin();
 
     BOOST_TEST(*it == NON_ZERO_VAL);
@@ -43,8 +42,7 @@ BOOST_AUTO_TEST_CASE( deref_first_free ) {
     gic_derived<int> container;
 
     auto free_key = container.emplace(NON_ZERO_VAL_1);
-    auto key = container.emplace(NON_ZERO_VAL_2);
-    (void)key;
+    (void)container.emplace(NON_ZERO_VAL_2);
     container.remove(free_key);
     auto it = container.begin();
 
@@ -70,15 +68,89 @@ BOOST_AUTO_TEST_CASE( iter_swap ) {
 }
 
 
+// ===== Input Iterator concept =====
+
+#ifndef ASSERT_TRUE
+#define ASSERT_TRUE(x) BOOST_TEST(!!(x))
+#endif
+
+BOOST_AUTO_TEST_CASE( iterator_equality_on_empty ) {
+    gic_derived<int> container;
+    ASSERT_TRUE(container.begin() == container.end());
+    ASSERT_TRUE(container.cbegin() == container.cend());
+    ASSERT_TRUE(container.cend() == container.begin());
+
+    ASSERT_TRUE(container.begin() == container.begin());
+    ASSERT_TRUE(container.begin() == container.cbegin());
+
+    auto const& cc = container;
+    ASSERT_TRUE(cc.begin() == cc.end());
+}
+
+BOOST_AUTO_TEST_CASE( iterator_inequality ) {
+    gic_derived<int> container;
+    (void)container.emplace(NON_ZERO_VAL);
+    ASSERT_TRUE(container.begin() != container.end());
+    ASSERT_TRUE(container.cbegin() != container.cend());
+}
+
+BOOST_AUTO_TEST_CASE( iterator_incr_to_end ) {
+    gic_derived<int> container;
+    (void)container.emplace(NON_ZERO_VAL);
+
+    auto it = container.cbegin();
+    ASSERT_TRUE(it != container.cend());
+    ++it;
+    ASSERT_TRUE(it == container.cend());
+}
+
+BOOST_AUTO_TEST_CASE( iterator_incr_skips_free ) {
+    gic_derived<int> container;
+    (void)container.emplace(NON_ZERO_VAL_1);
+    auto free_key = container.emplace(NON_ZERO_VAL_1);
+    (void)container.emplace(NON_ZERO_VAL_2);
+    container.remove(free_key);
+
+    auto it = container.cbegin();
+    ASSERT_TRUE(it != container.cend());
+    BOOST_TEST(*it++ == NON_ZERO_VAL_1);
+
+    ASSERT_TRUE(it != container.cend());
+    BOOST_TEST(*it == NON_ZERO_VAL_2);
+}
+
+
+// ===== Output Iterator concept =====
+
+template<typename T>
+constexpr bool is_iter_writeable =
+        std::is_assignable_v<decltype(*std::declval<T>()), int>;
+
+BOOST_AUTO_TEST_CASE( iterator_write ) {
+    gic_derived<int> container;
+    auto key = container.emplace(NON_ZERO_VAL_1);
+    auto it = container.begin();
+    static_assert (is_iter_writeable<decltype(it)>);
+
+    *it = NON_ZERO_VAL_2;
+    ASSERT_TRUE(*container[key] == NON_ZERO_VAL_2);
+}
+
+BOOST_AUTO_TEST_CASE( const_iterator_is_not_writable ) {
+    gic_derived<int> container;
+    auto const& cc = container;
+    static_assert (!is_iter_writeable<decltype(container.cbegin())>);
+    static_assert (!is_iter_writeable<decltype(cc.begin())>);
+}
+
+
 // ===== Forward Iterator concept =====
 
-/*
-BOOST_AUTO_TEST_CASE( empty_container ) {
+BOOST_AUTO_TEST_CASE( iterator_default_construction ) {
     gic_derived<int> container;
-    BOOST_TEST(container.begin() == container.end());
-    BOOST_TEST(container.cbegin() == container.cend());
 
-    auto cc = std::as_const(container);
-    BOOST_TEST(cc.begin() == cc.end());
+    using iter_t = decltype(container)::iterator;
+    using const_iter_t = decltype(container)::const_iterator;
+    ASSERT_TRUE(iter_t{} == container.end());
+    ASSERT_TRUE(const_iter_t{} == container.cend());
 }
-//*/
